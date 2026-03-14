b65fec5ad1abefb3c6a8a1dc90b5411a187cf5b8
RupendraGoku <116149190+RupendraGoku@users.noreply.github.com>
2026-03-14 13:18:12 +0530

    Replace Twitter icon with Youtube in footerContent

--- src/content/footerContent.js
+++ src/content/footerContent.js
@@ -1,4 +1,4 @@
-import { Facebook, Instagram, Twitter,Linkedin } from "lucide-react";
+import { Facebook, Instagram, Youtube,Linkedin } from "lucide-react";
 
 export const footerContent = {
   brand: {
